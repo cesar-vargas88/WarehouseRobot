56944d796d8594ead01c7af92da987ded67b9fa5
cesar-vargas88 <vargas.cesar88@gmail.com>
2017-08-05 23:58:16 -0500

    FreeRTOS Implemented.

--- WarehouseRobot.ino
+++ WarehouseRobot.ino
@@ -1,7 +1,10 @@
 
+#include <Arduino_FreeRTOS.h>
 #include "Car4W.h"
 #include "TCRT5000.h"
 #include "MotorCD.h"
+
+void TaskLineFollower(void *pvParameters);
 
 TCRT5000 Sensor1(31);
 TCRT5000 Sensor2(29);
@@ -20,37 +23,45 @@ int nSpeedMotorFrontRight = 0;
 int nSpeedMotorRearRight  = 0;
 
 bool  bSensor[5]  = {false, false, false, false, false};
-int nSpeed = 127;
+int nSpeed = 255;
 
 //////////////////////
 //  Variables PID   //
 //////////////////////
 
-int TiempoMuestreo = 1;        // tiempo de muestreo Se encuentra en milisegundos
-unsigned long pasado = 0;       // tiempo pasado (Se hace para asegurar tiempo de muestreo)
-unsigned long ahora;
+int SamplingTime = 1;        // tiempo de muestreo Se encuentra en milisegundos
+unsigned long Past = 0;       // tiempo Past (Se hace para asegurar tiempo de muestreo)
+unsigned long Now;
 float Ref = 0;                  // referencia 
 double Y = 0;                   // Salida
-double error;                   // error
-double errorPass = 0;           // Suma del error para la parte integral
-double errorAnt = 0;            // error anterior para la derivada
-double errorD = 0;
+double Error;                   // Error
+double SumError = 0;           // Suma del Error para la parte integral
+double LastError = 0;            // Error anterior para la derivada
+double DerivateError = 0;
 
 double U = 0;                   // Señal control
 
-int CambioTiempo = 0;
+int TimeChange = 0;
 
 float P = 0;                    // control proporcional
 float I = 0;                    // control Integral
 float D = 0;
       
 // constantes del controlador 
-float Kp = 25;
-float Ki = 2;
-float Kd = 25;
+float Kp = 40;
+float Ki = 4;
+float Kd = 40;
 
 void setup() 
-{
+{  
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  
+  while (!Serial) 
+  {
+    ; // wait for serial port to connect. Needed for native USB, on LEONARDO, MICRO, YUN, and other 32u4 based boards.
+  }
+
   Sensor1.Setup();
   Sensor2.Setup();
   Sensor3.Setup();
@@ -61,105 +72,119 @@ void setup()
   MotorFrontRight.Setup();    
   MotorRearLeft.Setup();      
   MotorRearRight.Setup();     
-  
-  Serial.begin(9600);
+
+  xTaskCreate(
+    TaskLineFollower
+    ,  (const portCHAR *) "LineFollower"
+    ,  128  // Stack size
+    ,  NULL
+    ,  1  // Priority
+    ,  NULL );
+
+  // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
 }
 
-void loop() 
+void loop()
 {
-  bSensor[0] = Sensor1.getDigitalValue();
-  bSensor[1] = Sensor2.getDigitalValue();
-  bSensor[2] = Sensor3.getDigitalValue();
-  bSensor[3] = Sensor4.getDigitalValue();
-  bSensor[4] = Sensor5.getDigitalValue();
+  // Empty. Things are done in Tasks.
+}
 
-  if( (bSensor[0] == 0) && (bSensor[1] == 0) && (bSensor[2] == 0) && (bSensor[3] == 0) && (bSensor[4] == 0) )           error = error;
-  else
+void TaskLineFollower(void *pvParameters)  // This is a task.
+{
+  (void) pvParameters;
+
+  for (;;)
   {
-    if     ( (bSensor[0] == 0) && (bSensor[1] == 0) && (bSensor[2] == 0) && (bSensor[3] == 0) && (bSensor[4] == 1) )    error =  4;
-    else if( (bSensor[0] == 0) && (bSensor[1] == 0) && (bSensor[2] == 0) && (bSensor[3] == 1) && (bSensor[4] == 1) )    error =  3;
-    else if( (bSensor[0] == 0) && (bSensor[1] == 0) && (bSensor[2] == 0) && (bSensor[3] == 1) && (bSensor[4] == 0) )    error =  2;
-    else if( (bSensor[0] == 0) && (bSensor[1] == 0) && (bSensor[2] == 1) && (bSensor[3] == 1) && (bSensor[4] == 0) )    error =  1;
-    else if( (bSensor[0] == 0) && (bSensor[1] == 0) && (bSensor[2] == 1) && (bSensor[3] == 0) && (bSensor[4] == 0) )    error =  0;
-    else if( (bSensor[0] == 0) && (bSensor[1] == 1) && (bSensor[2] == 1) && (bSensor[3] == 0) && (bSensor[4] == 0) )    error = -1;
-    else if( (bSensor[0] == 0) && (bSensor[1] == 1) && (bSensor[2] == 0) && (bSensor[3] == 0) && (bSensor[4] == 0) )    error = -2;
-    else if( (bSensor[0] == 1) && (bSensor[1] == 1) && (bSensor[2] == 0) && (bSensor[3] == 0) && (bSensor[4] == 0) )    error = -3;
-    else if( (bSensor[0] == 1) && (bSensor[1] == 0) && (bSensor[2] == 0) && (bSensor[3] == 0) && (bSensor[4] == 0) )    error = -4;
-  }
-  
-  ahora = millis();
-
-  CambioTiempo = ahora - pasado;                        // Diferencia de tiempo actual- pasado
-  
-  if(CambioTiempo >= TiempoMuestreo)                    // si se supera el tiempo de muestreo
-  {
-    errorPass = errorPass + (error * TiempoMuestreo);   // Cálculo de aproximación del area
-    errorD    = (error - errorAnt) / TiempoMuestreo;    // Cálculo Error Derivativo 
-      
-    P = Kp * error;                                     // Control Proporcional
-    I = Ki * errorPass;                                 // Control Integral
-    D = Kd * errorD;                                    // Control Derivativo
-      
-    U = P + I + D;                                      // Señal de control
-      
-    pasado = ahora;                                     // actualizar tiempo
-    errorAnt = error;                                   // actualizar el error     
-  }  
-
-  nSpeedMotorFrontLeft   = nSpeed - U;
-  nSpeedMotorRearLeft    = nSpeed - U;
-  nSpeedMotorFrontRight  = nSpeed + U;
-  nSpeedMotorRearRight   = nSpeed + U;
-
-  if(nSpeedMotorFrontLeft   >  255)   nSpeedMotorFrontLeft  =  255;
-  if(nSpeedMotorFrontLeft   < -255)   nSpeedMotorFrontLeft  = -255;
-  if(nSpeedMotorRearLeft    >  255)   nSpeedMotorRearLeft   =  255;
-  if(nSpeedMotorRearLeft    < -255)   nSpeedMotorRearLeft   = -255;
-  if(nSpeedMotorFrontRight  >  255)   nSpeedMotorFrontRight =  255;
-  if(nSpeedMotorFrontRight  < -255)   nSpeedMotorFrontRight = -255;
-  if(nSpeedMotorRearRight   >  255)   nSpeedMotorRearRight  =  255;
-  if(nSpeedMotorRearRight   < -255)   nSpeedMotorRearRight  = -255;
+    bSensor[0] = Sensor1.getDigitalValue();
+    bSensor[1] = Sensor2.getDigitalValue();
+    bSensor[2] = Sensor3.getDigitalValue();
+    bSensor[3] = Sensor4.getDigitalValue();
+    bSensor[4] = Sensor5.getDigitalValue();
     
-  if(nSpeedMotorFrontLeft >= 0)   MotorFrontLeft.Forward(nSpeedMotorFrontLeft);
-  else                            MotorFrontLeft.Backward(-1*nSpeedMotorFrontLeft);       
-
-  if(nSpeedMotorRearLeft >= 0)    MotorRearLeft.Forward(nSpeedMotorRearLeft);
-  else                            MotorRearLeft.Backward(-1*nSpeedMotorRearLeft);     
-
-  if(nSpeedMotorFrontRight >= 0)  MotorFrontRight.Forward(nSpeedMotorFrontRight);
-  else                            MotorFrontRight.Backward(-1*nSpeedMotorFrontRight);     
-
-  if(nSpeedMotorRearRight >= 0)   MotorRearRight.Forward(nSpeedMotorRearRight);
-  else                            MotorRearRight.Backward(-1*nSpeedMotorRearRight);     
-
-  Serial.print(bSensor[0]);
-  Serial.print(bSensor[1]);
-  Serial.print(bSensor[2]);
-  Serial.print(bSensor[3]);
-  Serial.print(bSensor[4]);
-  Serial.print("  EP:");
-  Serial.print(errorPass);
-  Serial.print("  ED:");
-  Serial.print(errorD);
-  Serial.print("  EA:");
-  Serial.print(errorAnt);
-  Serial.print("  P:");
-  Serial.print(P);
-  Serial.print("  I:");
-  Serial.print(I);
-  Serial.print("  D:");
-  Serial.print(D);
-  Serial.print("  U:");
-  Serial.print(U);
-  Serial.print("  MFL:");
-  Serial.print(nSpeedMotorFrontLeft);  
-  Serial.print("  MRL:");
-  Serial.print(nSpeedMotorRearLeft);
-  Serial.print("  MFR:");
-  Serial.print(nSpeedMotorFrontRight);
-  Serial.print("  MRR:");
-  Serial.println(nSpeedMotorRearRight);
-
-  //delay(500);
+    if     ( (bSensor[0] == 0) && (bSensor[1] == 0) && (bSensor[2] == 0) && (bSensor[3] == 0) && (bSensor[4] == 1) )    Error =  4;
+    else if( (bSensor[0] == 0) && (bSensor[1] == 0) && (bSensor[2] == 0) && (bSensor[3] == 1) && (bSensor[4] == 1) )    Error =  3;
+    else if( (bSensor[0] == 0) && (bSensor[1] == 0) && (bSensor[2] == 0) && (bSensor[3] == 1) && (bSensor[4] == 0) )    Error =  2;
+    else if( (bSensor[0] == 0) && (bSensor[1] == 0) && (bSensor[2] == 1) && (bSensor[3] == 1) && (bSensor[4] == 0) )    Error =  1;
+    else if( (bSensor[0] == 0) && (bSensor[1] == 0) && (bSensor[2] == 1) && (bSensor[3] == 0) && (bSensor[4] == 0) )    Error =  0;
+    else if( (bSensor[0] == 0) && (bSensor[1] == 1) && (bSensor[2] == 1) && (bSensor[3] == 0) && (bSensor[4] == 0) )    Error = -1;
+    else if( (bSensor[0] == 0) && (bSensor[1] == 1) && (bSensor[2] == 0) && (bSensor[3] == 0) && (bSensor[4] == 0) )    Error = -2;
+    else if( (bSensor[0] == 1) && (bSensor[1] == 1) && (bSensor[2] == 0) && (bSensor[3] == 0) && (bSensor[4] == 0) )    Error = -3;
+    else if( (bSensor[0] == 1) && (bSensor[1] == 0) && (bSensor[2] == 0) && (bSensor[3] == 0) && (bSensor[4] == 0) )    Error = -4;
+  
+    Now = millis();
+  
+    TimeChange = Now - Past;                        // Diferencia de tiempo actual- Past
+    
+    if(TimeChange >= SamplingTime)                    // si se supera el tiempo de muestreo
+    {
+      SumError = SumError + (Error * SamplingTime);   // Cálculo de aproximación del area
+      DerivateError    = (Error - LastError) / SamplingTime;    // Cálculo Error Derivativo 
+        
+      P = Kp * Error;                                     // Control Proporcional
+      I = Ki * SumError;                                 // Control Integral
+      D = Kd * DerivateError;                                    // Control Derivativo
+        
+      U = P + I + D;                                      // Señal de control
+        
+      Past = Now;                                     // actualizar tiempo
+      LastError = Error;                                   // actualizar el Error     
+    }  
+  
+    nSpeedMotorFrontLeft   = nSpeed - U;
+    nSpeedMotorRearLeft    = nSpeed - U;
+    nSpeedMotorFrontRight  = nSpeed + U;
+    nSpeedMotorRearRight   = nSpeed + U;
+  
+    if(nSpeedMotorFrontLeft   >  255)   nSpeedMotorFrontLeft  =  255;
+    if(nSpeedMotorFrontLeft   < -255)   nSpeedMotorFrontLeft  = -255;
+    if(nSpeedMotorRearLeft    >  255)   nSpeedMotorRearLeft   =  255;
+    if(nSpeedMotorRearLeft    < -255)   nSpeedMotorRearLeft   = -255;
+    if(nSpeedMotorFrontRight  >  255)   nSpeedMotorFrontRight =  255;
+    if(nSpeedMotorFrontRight  < -255)   nSpeedMotorFrontRight = -255;
+    if(nSpeedMotorRearRight   >  255)   nSpeedMotorRearRight  =  255;
+    if(nSpeedMotorRearRight   < -255)   nSpeedMotorRearRight  = -255;
+      
+    if(nSpeedMotorFrontLeft >= 0)   MotorFrontLeft.Forward(nSpeedMotorFrontLeft);
+    else                            MotorFrontLeft.Backward(-1*nSpeedMotorFrontLeft);       
+  
+    if(nSpeedMotorRearLeft >= 0)    MotorRearLeft.Forward(nSpeedMotorRearLeft);
+    else                            MotorRearLeft.Backward(-1*nSpeedMotorRearLeft);     
+  
+    if(nSpeedMotorFrontRight >= 0)  MotorFrontRight.Forward(nSpeedMotorFrontRight);
+    else                            MotorFrontRight.Backward(-1*nSpeedMotorFrontRight);     
+  
+    if(nSpeedMotorRearRight >= 0)   MotorRearRight.Forward(nSpeedMotorRearRight);
+    else                            MotorRearRight.Backward(-1*nSpeedMotorRearRight);     
+  
+    Serial.print(bSensor[0]);
+    Serial.print(bSensor[1]);
+    Serial.print(bSensor[2]);
+    Serial.print(bSensor[3]);
+    Serial.print(bSensor[4]);
+    Serial.print("  EP:");
+    Serial.print(SumError);
+    Serial.print("  ED:");
+    Serial.print(DerivateError);
+    Serial.print("  EA:");
+    Serial.print(LastError);
+    Serial.print("  P:");
+    Serial.print(P);
+    Serial.print("  I:");
+    Serial.print(I);
+    Serial.print("  D:");
+    Serial.print(D);
+    Serial.print("  U:");
+    Serial.print(U);
+    Serial.print("  MFL:");
+    Serial.print(nSpeedMotorFrontLeft);  
+    Serial.print("  MRL:");
+    Serial.print(nSpeedMotorRearLeft);
+    Serial.print("  MFR:");
+    Serial.print(nSpeedMotorFrontRight);
+    Serial.print("  MRR:");
+    Serial.println(nSpeedMotorRearRight);
+    
+    vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
+  }
 }
 
